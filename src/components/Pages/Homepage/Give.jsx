6a819b8b19450e5bebb7a70874986fd479d53ea2
Kwesi Hervie <herviek2001@gmail.com>
2023-12-29 16:22:46 +0100

    updates

--- src/components/Pages/Homepage/Give.jsx
+++ src/components/Pages/Homepage/Give.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import give from "../../../../public/assets/images/give.jpeg";
+import Image from "next/image";
 
 export const Give = () => {
   return (
@@ -48,9 +50,9 @@ export const Give = () => {
               </div>
             </div>
             <div className="relative mt-16 h-80 lg:mt-8">
-              <img
-                className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
-                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+              <Image
+                className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10 object-cover"
+                src={give}
                 alt="App screenshot"
                 width={1824}
                 height={1080}
