a3c148d517cf889b4faa4f96c6f9b72c54794d0a
Kwesi Hervie <herviek2001@gmail.com>
2023-12-29 16:48:27 +0100

    updates

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -20,6 +20,39 @@ import { AOSInit } from "../utils";
 
 import { openGraphImage } from "./shared-metadata";
 
+export const metadata = {
+  metadataBase: new URL("https://gt-website-nextjs.vercel.app"),
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-US",
+    },
+  },
+  openGraph: {
+    title: "Glory Tabernacle Ministry - Ojurin, Ibadan",
+    description: "Enter to worship, Depart to serve.",
+    url: "https://gt-website-nextjs.vercel.app/",
+    siteName: "Next.js",
+    images: [
+      {
+        url: "https://nextjs.org/og.png", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://nextjs.org/og-alt.png", // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  title: "Glory Tabernacle Ministry - Ojurin, Ibadan",
+  description: "Enter to worship, Depart to serve.",
+};
+
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
@@ -69,39 +102,22 @@ const corben = Corben({
 //   },
 // };
 
-export const metadata = {
-  metadataBase: new URL("https://gt-website-nextjs.vercel.app/"),
-  alternates: {
-    canonical: "/",
-    languages: {
-      "en-US": "/en-US",
-    },
-  },
-  openGraph: {
-    // images: "/assets/images/welcome.png",
-    images: [
-      {
-        url: "https://gt-website-nextjs.vercel.app/assets/images/welcome.png",
-        width: 800,
-        height: 600,
-      },
-      {
-        url: "https://gt-website-nextjs.vercel.app/assets/images/welcome.png",
-        width: 1800,
-        height: 1600,
-        alt: "My custom alt",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-  title: "Glory Tabernacle Ministry - Ojurin, Ibadan",
-  description: "Enter to worship, Depart to serve.",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta
+        property="og:image"
+        content="https://gt-website-nextjs.vercel.app/assets/images/welcome.png"
+      />
+      <meta property="og:image:type" content="<generated>" />
+      <meta property="og:image:width" content="<generated>" />
+      <meta property="og:image:height" content="<generated>" />
+
+      <meta name="twitter:image" content="<generated>" />
+      <meta name="twitter:image:type" content="<generated>" />
+      <meta name="twitter:image:width" content="<generated>" />
+      <meta name="twitter:image:height" content="<generated>" />
+
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <link
         rel="apple-touch-icon"
