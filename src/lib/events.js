707ef62522d78539c5aa1c4e94276589dcc564fd
Glory Tabernacle Ministry ICT Team <92822133+gtictunit@users.noreply.github.com>
2024-02-11 10:59:32 +0100

    updates

--- src/lib/events.js
+++ src/lib/events.js
@@ -21,58 +21,24 @@ export const eventList = [
     title: "Congregational Fasting and Prayer",
     allDay: true,
     start: new Date("2024-1-8"),
-    end: new Date("2024-1-28"),
+    end: new Date("2024-1-21"),
   },
-  //   {
-  //     title: "Long Event",
-  //     start: new Date(2015, 3, 7),
-  //     end: new Date(2015, 3, 10),
-  //   },
-
-  //   {
-  //     title: "DTS STARTS",
-  //     start: new Date(2016, 2, 13, 0, 0, 0),
-  //     end: new Date(2016, 2, 20, 0, 0, 0),
-  //   },
-
-  //   {
-  //     title: "DTS ENDS",
-  //     start: new Date(2016, 10, 6, 0, 0, 0),
-  //     end: new Date(2016, 10, 13, 0, 0, 0),
-  //   },
-
-  //   {
-  //     title: "Some Event",
-  //     start: new Date(2015, 3, 9, 0, 0, 0),
-  //     end: new Date(2015, 3, 9, 0, 0, 0),
-  //   },
-  //   {
-  //     title: "Conference",
-  //     start: new Date(2015, 3, 11),
-  //     end: new Date(2015, 3, 13),
-  //     desc: "Big conference for important people",
-  //   },
-  //   {
-  //     title: "Meeting",
-  //     start: new Date(2015, 3, 12, 10, 30, 0, 0),
-  //     end: new Date(2015, 3, 12, 12, 30, 0, 0),
-  //     desc: "Pre-meeting meeting, to prepare for the meeting",
-  //   },
-  //   {
-  //     title: "Lunch",
-  //     start: new Date(2015, 3, 12, 12, 0, 0, 0),
-  //     end: new Date(2015, 3, 12, 13, 0, 0, 0),
-  //     desc: "Power lunch",
-  //   },
-  //   {
-  //     title: "Meeting",
-  //     start: new Date(2015, 3, 12, 14, 0, 0, 0),
-  //     end: new Date(2015, 3, 12, 15, 0, 0, 0),
-  //   },
-  //   {
-  //     title: "Happy Hour",
-  //     start: new Date(2015, 3, 12, 17, 0, 0, 0),
-  //     end: new Date(2015, 3, 12, 17, 30, 0, 0),
-  //     desc: "Most important meal of the day",
-  //   },
+  {
+    title: "Pioneer Associate Pastor's 70th Birthday Anniversary",
+    allDay: true,
+    start: new Date("2024-2-2"),
+    end: new Date("2024-2-4"),
+  },
+  {
+    title: "Membership Induction Course",
+    allDay: true,
+    start: new Date("2024-2-16"),
+    end: new Date("2024-2-17"),
+  }, 
+  {
+    title: "",
+    allDay: true,
+    start: new Date("2024-2-16"),
+    end: new Date("2024-2-17"),
+  },
 ];
